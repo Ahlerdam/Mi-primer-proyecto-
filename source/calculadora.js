f49e99813ba711e0e1d5ba14a8ab123d06f0c6a8
Adrian <mendozaadrian09@gmail.com>
2019-08-05 19:30:45 -0300

    Calculadora con bootstrap

---
+++ source/calculadora.js
@@ -0,0 +1,42 @@
+var primerValor = document.getElementById("elementoUno");
+var SegundoValor = document.getElementById("elementoDos");
+var botoncito = document.getElementById("botonDeSuma");
+var botonDeEnviar = document.getElementById("enviar");
+
+//botonDeEnviar.addEventListener("click");
+botoncito.addEventListener("click", Operacion);
+ 
+const usuario = document.getElementById("tipoDeOperacion");
+console.log(usuario.value);
+
+var Operaciones = {
+    Suma: "suma",
+    Resta: "resta",
+    Multiplicacion: "multiplicar",
+    Dividir: "dividir"
+};
+
+function Operacion() {
+    
+    switch(usuario.value)
+    {
+        case Operaciones.Suma:
+        var resultSuma = parseInt(primerValor.value)+ parseInt(SegundoValor.value);
+        return alert (resultSuma);
+        break;
+        case Operaciones.Resta:
+        var resultResta = primerValor.value - SegundoValor.value;
+        return alert(resultResta);
+        break;
+        case Operaciones.Multiplicacion:
+        var resultMultiplicar = primerValor.value * SegundoValor.value;
+        return alert(resultMultiplicar);
+        break;
+        case Operaciones.Dividir:
+        var resultDividir = primerValor.value / SegundoValor.value;
+        return alert(resultDividir);
+        break;
+    }
+    console.log(primerValor.value, SegundoValor.value);
+};
+
